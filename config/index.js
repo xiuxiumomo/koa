90a81cf52d06dfcccb379e7e34c5bf9153fe72e3
xiuxiumomo <2331396362@qq.com>
2020-04-28 09:40:01 +0800

    调整

--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@ var development_env = require('./development');
 var production_env = require('./production');
 
 //根据不同的NODE_ENV，输出不同的配置对象，默认输出development的配置对象
-let isDev = process.env.NODE_ENV.includes('development');
-const currEnv = isDev ? development_env : production_env;
-module.exports = currEnv;
+// let isDev = process.env.NODE_ENV.includes('development');
+// const currEnv = isDev ? development_env : production_env;
+module.exports = production_env;
