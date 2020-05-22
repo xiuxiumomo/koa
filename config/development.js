e802161a6f6a696799a3661d5453d7c9ae85471d
xiuxiumomo <2331396362@qq.com>
2020-05-22 14:00:59 +0800

    添加部分

--- config/development.js
+++ config/development.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: 'development', //环境名称
-    port: 3000,         //服务端口号
+    port: 3300,         //服务端口号
     dbName: 'koa',    //数据库名字
     dbUserName: 'root', //用户名
     dbPassWord: '123456',
--- config/production.js
+++ config/production.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: 'production', //环境名称
-    port: 3000,         //服务端口号
+    port: 3300,         //服务端口号
     dbName: 'koa',    //数据库名字
     dbUserName: 'root', //用户名
     dbPassWord: '123456', //数据库密码
