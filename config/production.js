a770708753d4b34cb6a124038f0c3cbe327393dc
xiuxiumomo <2331396362@qq.com>
2020-06-23 22:00:14 +0800

    product文件

--- config/production.js
+++ config/production.js
@@ -1,8 +1,8 @@
 module.exports = {
     env: 'production', //环境名称
     port: 3000,         //服务端口号
-    dbName: 'koa',    //数据库名字
+    dbName: 'koa_api',    //数据库名字
     dbUserName: 'root', //用户名
     dbPassWord: '123456', //数据库密码
-    dbIP: '193.112.176.177' //地址
+    dbIP: '95.169.7.64' //地址
 }
